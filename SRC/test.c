12b067d806053500129b81e05c296873cb35b6eb
99003754 <78854545+99003754@users.noreply.github.com>
2021-02-22 09:54:51 +0530

    Create test.c

---
+++ SRC/test.c
@@ -0,0 +1,29 @@
+#include "unity.h"
+#include "main.h"
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/*void test_factorial_positive_num(void)
+{
+    long result = 1;
+    TEST_ASSERT_EQUAL(SUCCESS, find_factorial(10,&result));
+    TEST_ASSERT_EQUAL(3628800, result);
+}
+void test_factorial_negative_num(void)
+{
+    long result = 1;
+    TEST_ASSERT_EQUAL(SUCCESS, find_factorial(10,&result));
+}
+int main(void)
+{
+
+  UNITY_BEGIN();
+  
+  RUN_TEST(test_factorial_positive_num);
+ 
+  return UNITY_END();
+}
+*/
