b73ee1e89376c3003ca8fdb60dedc582b8b279b3
99003751 <78887234+99003751@users.noreply.github.com>
2021-02-22 09:32:12 +0530

    added main.h file

---
+++ INC/main.h
@@ -0,0 +1,28 @@
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+
+void insert(int box_id, char* color, float length,float breadth,float height,float weight);
+double avr_volume();
+double diff_min_max_volume();
+int box_max_height();
+int box_same_color(char* color);
+void search(int box_id);
+void Delete(int box_id);
+void display();
+void update_weight(int box_id);
+
+
+
+struct Box
+{
+    int box_id;
+    char color[100];
+   // char phone[100];
+    float length;
+    float breadth;
+    float height;
+    float weight;
+    struct Box *next;
+
+}* head;
